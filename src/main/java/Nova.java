77ede8737b43dee7c19a050f8a63d9a3655b9ec2
jhu0748 <jackson.j.hu@gmail.com>
2025-02-05 00:05:41 +0800

    Changed chatbot name, week2

---
+++ src/main/java/Nova.java
@@ -0,0 +1,8 @@
+public class Nova {
+    public static void main(String[] args) {
+        String logo = "Nova";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
