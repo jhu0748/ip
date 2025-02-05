3d85677ac8a5fb9f791887dd63691707e045cabd
jhu0748 <jackson.j.hu@gmail.com>
2025-02-05 17:08:40 +0800

    Week 3 Level-1: Echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void main(String[] args) {
         String logo = "Nova";
         System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while(true) {
+            input = scanner.nextLine();
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("     " + input);
+            }
+        }
+        scanner.close();
     }
 }
