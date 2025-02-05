1e49d156c93a9b5cce70bdcfe8781e745e4ce6ad
jhu0748 <jackson.j.hu@gmail.com>
2025-02-05 18:36:57 +0800

    Week 3 Level-2: Add, List

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -3,19 +3,37 @@ import java.util.Scanner;
 public class Nova {
     public static void main(String[] args) {
         String logo = "Nova";
-        System.out.println("Hello! I'm " + logo);
-        System.out.println("What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm " + logo);
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] tasks = new String[100];
+        int numTasks = 0;
+
         while(true) {
             input = scanner.nextLine();
-            if(input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+            if(input.equals("bye")) { // exit
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
                 break;
+            } else if(input.equals("list")) { // display task list
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < numTasks; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("    ____________________________________________________________");
             } else {
-                System.out.println("     " + input);
+                tasks[numTasks] = input;
+                numTasks++;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + input);
+                System.out.println("    ____________________________________________________________");
             }
         }
+
         scanner.close();
     }
 }
