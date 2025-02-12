2fb6ff9268c9abefaf0ee0ea950010bd06092b34
jhu0748 <jackson.j.hu@gmail.com>
2025-02-12 21:40:15 +0800

    Level-4

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -2,75 +2,43 @@ import java.util.Scanner;
 
 public class Nova {
     public static void main(String[] args) {
-        String logo = "Nova";
-        printSeparatorLine();
-        System.out.println("\t Hello! I'm " + logo);
-        System.out.println("\t What can I do for you?");
-        printSeparatorLine();
+        TaskList taskList = new TaskList();
+        System.out.println("\t Hello! I'm Nova.\n\t What can I do for you?");
+        
         Scanner scanner = new Scanner(System.in);
-        String[] taskList = new String[100];
-        int numTasks = 0;
         String input;
 
-        input_logic:
         while(true) {
             input = scanner.nextLine();
-            String[] inputSplit = input.split(" ",2);
-            String action = inputSplit[0];
-            switch (action) {
-            case "bye":  // exit
-                printSeparatorLine();
+            if (input.equals("bye")) {
+                Ui.printSeparatorLine();
                 System.out.println("\t Bye. Hope to see you again soon!");
-                printSeparatorLine();
-                break input_logic;
-            case "list":  // display task list
-                printSeparatorLine();
-                for (int i = 0; i < numTasks; i++) {
-                    System.out.println("     " + (i + 1) + ". " + taskList[i]);
-                }
-                printSeparatorLine();
+                Ui.printSeparatorLine();
                 break;
-            case "mark": { // mark task as done
-                int taskNum = Integer.parseInt(inputSplit[1]) - 1; // idx val in task array to be marked done
-
-                printSeparatorLine();
-                if (taskNum >= 0 && taskNum < numTasks) {
-                    System.out.println("\t Nice! I've marked this task as done:");
-                    System.out.println("\t [X] " + taskList[taskNum]);
-                } else {
-                    System.out.println("\t Invalid task number.");
-                }
-                printSeparatorLine();
-                break;
-            }
-            case "unmark": { // unmark task
-                int taskNum = Integer.parseInt(inputSplit[1]) - 1;
-                printSeparatorLine();
-                if (taskNum >= 0 && taskNum < numTasks) {
-                    System.out.println("\t Ok, I've marked this task as not done yet:");
-                    System.out.println("\t [ ] " + taskList[taskNum]);
-                } else {
-                    System.out.println("\t Invalid task number.");
-                }
-                printSeparatorLine();
-                break;
-            }
-            default:
-                taskList[numTasks] = input;
-                numTasks++;
-                printSeparatorLine();
-                System.out.println("\t added: " + input);
-                printSeparatorLine();
-                break;
+            } else if (input.equals("list")) {
+                taskList.listTasks();
+            } else if (input.startsWith("todo")) {
+                String description = input.substring(5);
+                taskList.addTask(new Todo(description));
+            } else if (input.startsWith("deadline")) {
+                String[] parts = input.substring(9).split(" /by ");
+                taskList.addTask(new Deadline(parts[0], parts[1]));
+            } else if (input.startsWith("event")) {
+                String[] parts = input.substring(6).split(" /from | /to ");
+                taskList.addTask(new Event(parts[0], parts[1], parts[2]));
+            } else if(input.startsWith("mark")) {
+                int taskIndexMark = Integer.parseInt(input.substring(5)) - 1;
+                taskList.markTaskDone(taskIndexMark);
+            } else if(input.startsWith("unmark")) {
+                int taskIndexUnmark = Integer.parseInt((input.substring(7))) - 1;
+                taskList.unmarkTaskDone(taskIndexUnmark);
+            } else {
+                Ui.printSeparatorLine();
+                System.out.println("Unrecognized command, please try again.");
+                Ui.printSeparatorLine();
             }
         }
-
         scanner.close();
-    }
-
-    // helper method to print lines separating user input and command output
-    private static void printSeparatorLine() {
-        System.out.println("\t  ____________________________________________________________");
     }
 
 }
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,45 @@
+public class TaskList {
+    private final Task[] taskList = new Task[100];
+    private int numTasks = 0;
+
+    public void addTask(Task task) {
+        taskList[numTasks] = task;
+        numTasks++;
+        Ui.printSeparatorLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println("\t  " + task);
+        System.out.println("\t Now you have " + numTasks + " tasks in the list.");
+        Ui.printSeparatorLine();
+    }
+
+    public void markTaskDone(int taskNum) {
+        taskList[taskNum].markAsDone();
+        Ui.printSeparatorLine();
+        System.out.println("\t Nice! I've marked this task as done:");
+        System.out.println(("\t   " + taskList[taskNum]));
+        Ui.printSeparatorLine();
+    }
+
+    public void unmarkTaskDone(int taskNum) {
+        taskList[taskNum].unmarkAsDone();
+        Ui.printSeparatorLine();
+        System.out.println("\t Ok! I've marked this task as not done yet:");
+        System.out.println(("\t   " + taskList[taskNum]));
+        Ui.printSeparatorLine();
+    }
+
+    public void listTasks() {
+        Ui.printSeparatorLine();
+
+        if(numTasks == 0) {
+            System.out.println("\t Sorry! No tasks were recorded. Try adding to the task list.");
+        } else {
+            System.out.println("\t Here are the tasks in your list:");
+            for (int i = 0; i < numTasks; i++) {
+                System.out.println("\t " + (i + 1) + "." + taskList[i]);
+            }
+        }
+
+        Ui.printSeparatorLine();
+    }
+}
