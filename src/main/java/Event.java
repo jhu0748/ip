1cf21d4011631b0ac586d43e1ba3b132308c908b
jhu0748 <jackson.j.hu@gmail.com>
2025-02-12 23:23:18 +0800

    Level-5

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,6 +4,18 @@ public class Event extends Task {
 
     public Event(String description, String from, String to) {
         super(description);
+        if (description == null || description.trim().isEmpty()) {
+            throw new IllegalArgumentException("OOPS!!! Sorry, but the description of an event cannot be empty.");
+        }
+        if (from == null || from.trim().isEmpty()) {
+            throw new IllegalArgumentException("OOPS!!! Sorry, but the event must have a valid /from date.");
+        }
+        if (to == null || to.trim().isEmpty()) {
+            throw new IllegalArgumentException("OOPS!!! Sorry, but the event must have a valid /to date.");
+        }
+        if(description.charAt(0) != ' ') {
+            throw new IllegalArgumentException(("OOPS!! Sorry, but that is an unrecognized command."));
+        }
         this.from = from;
         this.to = to;
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,6 +1,13 @@
 public class Todo extends Task {
     public Todo(String description) {
-        super(description);
+        super(description.trim());
+
+        if(description.trim().isEmpty()) {
+            throw new IllegalArgumentException("OOPS!! Sorry, but the todo description can't be empty. Please include a description and try again.");
+        }
+        if(description.charAt(0) != ' ') {
+            throw new IllegalArgumentException(("OOPS!! Sorry, but that is an unrecognized command."));
+        }
     }
 
     @Override
