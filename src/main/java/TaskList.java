cfedf3bfd847882b2aec1b4d57fd1aaebd3c669a
jhu0748 <jackson.j.hu@gmail.com>
2025-02-19 15:05:40 +0800

    A-Collections

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,43 +1,43 @@
+import java.util.ArrayList;
+
 public class TaskList {
-    private final Task[] taskList = new Task[100];
-    private int numTasks = 0;
+    private static final ArrayList<Task> taskArrayList = new ArrayList<>();
 
     // add task to tasklist
     public void addTask(Task task) throws NovaException {
-        taskList[numTasks] = task;
-        numTasks++;
+        taskArrayList.add(task);
         Ui.printSeparatorLine();
         System.out.println("\t Got it. I've added this task:");
         System.out.println("\t  " + task);
-        System.out.println("\t Now you have " + numTasks + " tasks in the list.");
+        System.out.println("\t Now you have " + taskArrayList.size() + " tasks in the list.");
         Ui.printSeparatorLine();
     }
     // mark task as done [X]
     public void markTaskDone(int taskNum) {
-        taskList[taskNum].markAsDone();
+        taskArrayList.get(taskNum).markAsDone();
         Ui.printSeparatorLine();
         System.out.println("\t Nice! I've marked this task as done:");
-        System.out.println(("\t   " + taskList[taskNum]));
+        System.out.println("\t   " + taskArrayList.get(taskNum));
         Ui.printSeparatorLine();
     }
     // mark as not done [ ]
     public void unmarkTaskDone(int taskNum) {
-        taskList[taskNum].unmarkAsDone();
+        taskArrayList.get(taskNum).unmarkAsDone();
         Ui.printSeparatorLine();
         System.out.println("\t Ok! I've marked this task as not done yet:");
-        System.out.println(("\t   " + taskList[taskNum]));
+        System.out.println("\t   " + taskArrayList.get(taskNum));
         Ui.printSeparatorLine();
     }
     // print out tasklist if there is at least 1 task in the list
     public void listTasks() {
         Ui.printSeparatorLine();
 
-        if(numTasks == 0) {
+        if(taskArrayList.isEmpty()) {
             System.out.println("\t Sorry! No tasks were recorded. Try adding to the task list.");
         } else {
             System.out.println("\t Here are the tasks in your list:");
-            for (int i = 0; i < numTasks; i++) {
-                System.out.println("\t " + (i + 1) + "." + taskList[i]);
+            for (int i = 0; i < taskArrayList.size(); i++) {
+                System.out.println("\t " + (i + 1) + "." + taskArrayList.get(i));
             }
         }
         Ui.printSeparatorLine();
